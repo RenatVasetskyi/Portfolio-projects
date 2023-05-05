8c0817c697f56591df648437e8ce12d8f9e59740
Renat Vasetskyi <renat.zayika@gmail.com>
2023-05-05 22:30:57 +0300

    improved

--- Tower Defence/Assets/Scripts/Boosters/DestroyBooster.cs
+++ Tower Defence/Assets/Scripts/Boosters/DestroyBooster.cs
@@ -5,11 +5,23 @@ namespace Assets.Scripts.Boosters
     public class DestroyBooster : MonoBehaviour
     {
         [SerializeField] private Booster _booster;
+        [SerializeField] private GameObject _destroyMeteorEffect;
+
+        private float _destroyEffectDelay = 1f;
 
         private void Awake() =>
             _booster.OnTargetReached += Destroy;
 
-        private void Destroy() =>
+        private void Destroy()
+        {
             Destroy(gameObject);
+            PlayEffect();
+        }
+
+        public void PlayEffect()
+        {
+            GameObject effect = Instantiate(_destroyMeteorEffect, transform.position, transform.rotation);
+            Destroy(effect, _destroyEffectDelay);
+        }
     }
 }
