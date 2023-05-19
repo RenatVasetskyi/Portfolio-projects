b7607cd9ded4ddfa88aca88f60cba5e674ae9051
Renat <renat.zayika@gmail.com>
2023-05-19 13:46:35 +0200

    imrpoved victory state

--- Tower Defence/Assets/Scripts/Architecture/States/VictoryState.cs
+++ Tower Defence/Assets/Scripts/Architecture/States/VictoryState.cs
@@ -1,7 +1,8 @@
-using Assets.Scripts.Architecture.Services.Factories.UI;
 using Assets.Scripts.Architecture.Services.Interfaces;
 using Assets.Scripts.Architecture.States.Interfaces;
 using Assets.Scripts.Audio;
+using Assets.Scripts.Data;
+using Assets.Scripts.Data.Levels;
 using Assets.Scripts.Data.Windows;
 
 namespace Assets.Scripts.Architecture.States
@@ -10,11 +11,15 @@ namespace Assets.Scripts.Architecture.States
     {
         private readonly IWindowService _windowService;
         private readonly IAudioService _audioService;
+        private readonly IAssetProvider _assetProvider;
+        private readonly ICurrentLevelSettingsProvider _currentLevelSettingsProvider;
 
-        public VictoryState(IWindowService windowService, IAudioService audioService)
+        public VictoryState(IWindowService windowService, IAudioService audioService, IAssetProvider assetProvider, ICurrentLevelSettingsProvider currentLevelSettingsProvider)
         {
             _windowService = windowService;
             _audioService = audioService;
+            _assetProvider = assetProvider;
+            _currentLevelSettingsProvider = currentLevelSettingsProvider;
         }
         
         public void Exit()
@@ -23,9 +28,21 @@ namespace Assets.Scripts.Architecture.States
 
         public void Enter()
         {
+            OpenNextLevel();
             _audioService.StopMusic();
             _audioService.PlaySfx(SfxType.Win);
             _windowService.Open(WindowId.Victory);
+        }
+
+        private void OpenNextLevel()
+        {
+            AllLevelsSettings levels = _assetProvider.Initialize<AllLevelsSettings>(AssetPath.AllLevelsSettings);
+
+            for (int i = 0; i < levels.Levels.Count - 1; i++)
+            {
+                if (levels.Levels[i].Id == _currentLevelSettingsProvider.GetCurrentLevelSettings().Id)
+                    levels.Levels[i + 1].IsLevelOpened = true;
+            }
         }
     }
 }
